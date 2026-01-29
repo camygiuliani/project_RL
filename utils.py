c18ffc6bec222528f171fd52cec210397ad79ba9
Camilla Giuliani <camilla.giuliani@live.com>
2026-01-29 23:22:13 +0100

    new function for csv

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
 import yaml
+import os
+import pandas as pd
+from datetime import datetime
 
 
 #loading configuration from yaml file
 def load_config(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
+
+
+
+def save_training_csv(history,path):
+    if len(history) == 0:
+        print("[WARN] No metrics to save.")
+        return None
+
+
+    df = pd.DataFrame(history)
+    df.to_csv(path, index=False)
+    print(f"[TRAIN] Saved training metrics to: {path}")
+    return path
